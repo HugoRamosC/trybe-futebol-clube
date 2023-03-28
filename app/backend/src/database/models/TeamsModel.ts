0d215f5b9dbbbe5a8b585fb44658e900b6353138
HugoRamosC <hugor.castilho@gmail.com>
2023-03-28 18:35:48 -0300

    feat: create teams migration & model - req01

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,27 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  teamName: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  tableName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
