7b2b0bf6215028ef128306562b500f8819da27da
HugoRamosC <hugor.castilho@gmail.com>
2023-03-31 11:43:29 -0300

    refact: teams routes with bind

--- app/backend/src/routes/teams.routes.ts
+++ app/backend/src/routes/teams.routes.ts
@@ -7,7 +7,7 @@ const teamsRouters = Router();
 const teamsService = new TeamsService();
 const teamsController = new TeamsController(teamsService);
 
-teamsRouters.get('/teams', (req, res) => teamsController.getAll(req, res));
-teamsRouters.get('/teams/:id', (req, res) => teamsController.getById(req, res));
+teamsRouters.get('/teams', teamsController.getAll.bind(teamsController));
+teamsRouters.get('/teams/:id', teamsController.getById.bind(teamsController));
 
 export default teamsRouters;
